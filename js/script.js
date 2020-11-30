820c1e71a7e41bb2c89cf82a155fc2a00a05f774
sirQuiz Sergey <sergey.kovalchuk.2012@gmail.com>
2020-11-30 22:11:33 +0200

    added movie count

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-let b = 0;  
+let movieCount = prompt("How many films do you watch?", "0"); 
+
+let personalMovieDB = {
+    count  : movieCount,
+    movies : {},
+    actors : {},
+    genres : [],
+    privat : false
+};
+
+alert(personalMovieDB['count']);
