7473a27e518db3908c328e69a9735861675a3456
sirQuiz Sergey <sergey.kovalchuk.2012@gmail.com>
2020-12-06 10:38:42 +0200

    Added loop and conditions for questions

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-let moviesCount     = prompt("How many films do you watch?", "0"),
-    moviesWatched   = prompt("One of the last movies you wathced?", ""),
-    movieRate       = prompt("How did you rate it?", "");
-
+let moviesCount     = +prompt("How many films do you watch?", ""),
+    moviesWatched   = "",
+    movieRate       = "",
+    message         =  "";
 let personalMovieDB = {
     count  : moviesCount,
     movies : {},
@@ -9,7 +9,33 @@ let personalMovieDB = {
     genres : [],
     privat : false
 };
+if (moviesCount > 0) {
+    for(let i = 0; i < moviesCount; i++){
+        moviesWatched = prompt("One of the last movies you wathced?", ""),
+        movieRate     = prompt("How did you rate it?", "");
+        
+        if( moviesWatched != null && movieRate != null && moviesWatched != '' && movieRate != '' && moviesWatched.length != 0 && movieRate.length != 0 && moviesWatched.length < 50 && movieRate.length < 50 ){
+            personalMovieDB.movies[moviesWatched] = movieRate; 
+            console.log('done');           
+        } else {
+            console.log('error');
+            i--;
+        }
+    }
+} else {
+    do {
+        moviesCount = +prompt("How many films do you watch?", "");
+    } while(moviesCount < 1);
+}
 
-personalMovieDB.movies[moviesWatched] = movieRate;
+if (moviesCount < 10) {
+    message = "Few movies watched";
+} else if (moviesCount > 10 && moviesCount < 30){
+    message = "You are a classic spectator";
+} else {
+    message = "You are moviegoer";
+}
 
+console.log(message);
 console.log(personalMovieDB);
+
